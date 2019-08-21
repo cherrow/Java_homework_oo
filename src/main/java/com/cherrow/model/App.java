2f1386415bb1e7e6208c0d1dfec824a365a0bbe3
cherrow <cherrow@foxmail.com>
2019-08-21 20:32:07 +0800

    remove useless method

--- src/main/java/com/cherrow/model/App.java
+++ src/main/java/com/cherrow/model/App.java
@@ -1,11 +1,8 @@
 package com.cherrow.model;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
     }
 }
